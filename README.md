9ccdea6b1930451058408116d62ea40405dd0d6f
maxim <soulman.is.good@gmail.com>
2014-09-16 18:38:50 +0600

    Defer.when.push logic added

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
 node-defer
 ==========
 
-Deferred object with when implemintation
+Deferred object with when implementation
+
+## Simple usage
+
+```javascript
+var Deferred = require('node-defer');
+var a = new Deferred();
+var b = new Deferred();
+var c = new Deferred();
+Deferred.when.push(a.promise()).push([b,c])().then(
+    function(args_a, args_b, args_c)){
+        console.log("Success");
+    },
+    function(err){
+        console.log("Failure");
+    }
+);
+//for async simulation
+setTimeout(function(){
+    a.resolve("A");
+    b.resolve("B");
+    c.resolve("C");
+}, 1000);
+```
+
+## Deferred object methods
+
+# *defer.resolve(arguments)*
+
+Marks deferred instance as successfully resolved and calls all *success* callbacks.
+Returns same deferred instance.
+
+# *defer.reject(arguments)*
+
+Marks deferred instance as failed and calls all *error* callbacks.
+Returns same deferred instance.
+
+# *defer.success(callback)*
+
+Adds success callback on resolve.
+Will be called immediately if resolve already fired.
+Returns same deferred instance.
+
+# *defer.error(callback)*
+
+Adds error callback on reject.
+Will be called immediately if reject already fired.
+Returns same deferred instance.
+
+# *defer.then(successCallback, errorCallback)*
+
+Adds success and error callbacks respectively.
+Will be called immediately if resolve or reject already fired.
+Returns same deferred instance.
+
+# *defer.isDone()*
+
+Tells if instance already has been rejected or resolved.
+Returns boolean.
+
+# *defer.isRejected()*
+
+Tells if instance has been rejected or not.
+Returns boolean.
+
+# *defer.promise()*
+
+Returns same deferred instance.
+
+## Deferred.when function
+
+Aggregate promises stack resolve in one deferred instance
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var Deferred = function(initFunc){
+/**
+ * Deferred object
+ * Simple class to make promises
+ * @param function initFunc optional argument if you want to init something on call
+ * @returns {Deferred}
+ */
+var Deferred = function deferred (initFunc) {
   var doneCbs = [];
   var failCbs = [];
   var isFailed = false;
@@ -7,39 +13,46 @@ var Deferred = function(initFunc){
 
   typeof initFunc === 'function' && initFunc.call(this);
 
-  this.isRejected = function(){
-    return isFailed;
+  this.isDone = function () {
+    return isDone;
   };
 
-  this.promise = function(){
+  this.isRejected = function () {
+    return isDone && isFailed;
+  };
+
+  this.promise = function () {
     return this;
   };
 
-  this.reject = function(){
-    if(isDone || isFailed) return this;
+  this.reject = function () {
+    if (isDone || isFailed)
+      return this;
     isFailed = true;
     result_args = arguments;
-    while(failCbs.length > 0) {
+    while (failCbs.length > 0) {
       var cb = failCbs.shift();
       cb && cb.apply(this, result_args);
     }
     return this;
   };
 
-  this.resolve = function(){
-    if(isDone || isFailed) return this;
+  this.resolve = function () {
+    if (isDone || isFailed)
+      return this;
     isDone = true;
     result_args = arguments;
-    while(doneCbs.length > 0) {
+    while (doneCbs.length > 0) {
       var cb = doneCbs.shift();
       cb && cb.apply(this, result_args);
     }
     return this;
   };
 
-  this.success = this.done = function(cb){
-    typeof cb === 'function' || (cb = function(){});
-    if(isDone) {
+  this.success = this.done = function (cb) {
+    typeof cb === 'function' || (cb = function () {
+    });
+    if (isDone) {
       cb.call(this, result_args);
       return this;
     }
@@ -47,9 +60,10 @@ var Deferred = function(initFunc){
     return this;
   };
 
-  this.error = this.fail = function(cb){
-    typeof cb === 'function' || (cb = function(){});
-    if(isFailed) {
+  this.error = this.fail = function (cb) {
+    typeof cb === 'function' || (cb = function () {
+    });
+    if (isFailed) {
       cb.call(this, result_args);
       return this;
     }
@@ -57,14 +71,16 @@ var Deferred = function(initFunc){
     return this;
   };
 
-  this.then = function(doneCb, failCb){
-    typeof doneCb === 'function' || (doneCb = function(){});
-    typeof failCb === 'function' || (failCb = function(){});
-    if(isDone) {
+  this.then = function (doneCb, failCb) {
+    typeof doneCb === 'function' || (doneCb = function () {
+    });
+    typeof failCb === 'function' || (failCb = function () {
+    });
+    if (isDone) {
       doneCb.apply(this, result_args);
       return this;
     }
-    if(isFailed) {
+    if (isFailed) {
       failCb.apply(this, result_args);
       return this;
     }
@@ -75,40 +91,66 @@ var Deferred = function(initFunc){
 
   return this;
 };
+/**
+ * Convinient stack array for when.push(promise) logic
+ * @type Array
+ */
+Deferred.stack = [];
 
-Deferred.when = function(){
-  var args = arguments;
+/**
+ * Wait when all promises done .then...
+ * @returns {deferred}
+ */
+Deferred.when = function () {
+  var args = Array.prototype.slice.call(arguments).concat(Deferred.stack);
   var defer = new Deferred();
   var length = args.length;
   var isRejected = false;
-  var agrigated_results = [];
+  var aggregated_results = [];
 
-  setTimeout(function(){
-    for(i in args) {
-      if(isRejected) {
-          break;
+  //clear stack on call
+  Deferred.stack = [];
+
+  setTimeout(function () {
+    for (var i in args) {
+      if (isRejected) {
+        break;
       }
-      if(args[i] && typeof args[i].promise === 'function') {
-        args[i].then(function(){
+      if (args[i] && typeof args[i].promise === 'function') {
+        args[i].then(function () {
           length--;
-          agrigated_results.push(arguments);
-          if(!isRejected && length === 0) {
-            defer.resolve.apply(defer, agrigated_results);
+          aggregated_results.push(arguments);
+          if (!isRejected && length === 0) {
+            defer.resolve.apply(defer, aggregated_results);
           }
-        },function(){
+        }, function () {
           isRejected = true;
           defer.reject.apply(defer, arguments);
         });
       } else {
         length--;
-        if(length === 0) {
+        if (length === 0) {
           defer.reject(['Not a promise']);
         }
       }
     }
-  },0);
+  }, 0);
 
   return defer.promise();
+};
+
+/**
+ * Chainable promises stack aggregator
+ * @param {Deferred} promise
+ * @returns {Deferred.when}
+ */
+Deferred.when.push = function (promise) {
+  if(promise instanceof Array) {
+    Deferred.stack.push.apply(Deferred.stack, promise);
+  } else {
+    Deferred.stack.push(promise);
+  }
+  return Deferred.when;
 };
 
 module.exports = Deferred;
